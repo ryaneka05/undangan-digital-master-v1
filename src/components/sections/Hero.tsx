87e26a5496283f83084ce1fbdfa74a0731359761
ryan eka <ryanzzeka@gmail.com>
2025-11-15 15:29:48 +0700

    update save the date

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,5 +1,6 @@
 import OpeningVerse from "@/components/common/OpeningVerse";
 import Bride from "@/components/common/Bride";
+import SaveTheDate from "@/components/common/SaveTheDate";
 
 export default function Hero() {
     return (
