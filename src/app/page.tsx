04f5ff04a93afd6ca870d93d24aa915a48b36449
Ryan Eka <ryanzzeka@gmail.com>
2025-12-02 21:37:19 +0700

    update error deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+"use client"
 import theme from "@/config/theme"
 
+export const dynamic = "force-dynamic";
 export default async function Home() {
   const ThemePage = (await import(`./${theme.active}/page`)).default;
   return <ThemePage/>
